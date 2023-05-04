687abe420bb3b1ba320819ec5034ebd6e77f4db1
GriffinJack <griffinjacksanford@gmail.com>
2023-05-03 22:19:59 -0400

    Update houseScrape.py

--- houseScrape.py
+++ houseScrape.py
@@ -3,28 +3,31 @@ import requests
 import pandas as pd
 import numpy as np
 
+def HouseDetails(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find_all('ul', {'class' : 'property-features-list'})
+    labels = np.array([])
+    values = np.array([])
+
+    for i in results:
+        for j in i.find_all('span', {'class': 'value'}):
+            values = np.append(values, j.get_text())
+
+    for i in results:
+        for j in i.find_all('span', {'class': 'label'}):
+            labels = np.append(labels, j.get_text())
+
+    dict = {}
+
+    for A, B in zip(labels,values):
+        dict[A] = B
+
+    return(dict)
+
+
+
 URL = 'https://www.royallepage.ca/en/property/alberta/calgary/617-55-avenue-sw/19600830/mlsa2044095/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find_all('ul', {'class' : 'property-features-list'})
-
-labels = np.array([])
-values = np.array([])
-
-for i in results:
-    for j in i.find_all('span', {'class': 'value'}):
-        values = np.append(values, j.get_text())
-
-for i in results:
-    for j in i.find_all('span', {'class': 'label'}):
-        labels = np.append(labels, j.get_text())
-
-df = pd.DataFrame(columns=labels)
-df.loc[len(df)] = values
-print(df)
 
 
-
-
-#print(results)
+print(HouseDetails(URL))
